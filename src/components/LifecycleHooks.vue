bd749789338720b12a2e75db252b3551f12f1ac4
Gary11777 <iharushakou@gmail.com>
2026-09-22 09:23:14 +0300

    Lifecycle Hooks

---
+++ src/components/LifecycleHooks.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+})
+</script>
+
+<template>
+  <section class="container">
+    <header class="page-header">
+      <h1 class="underline">Lifecycle Hooks</h1>
+    </header>
+
+    <div class="box">
+      
+    </div>
+
+  </section>
+</template>
